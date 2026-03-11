51bb489ebc5899b6c05e405ba06da1b2d521c66b
yd4812-web <yd4812@srmist.edu.in>
2026-03-11 21:11:12 +0530

    UC4

--- oopsbannerapp.java
+++ oopsbannerapp.java
@@ -1,24 +1,17 @@
 public class oopsbannerapp {
 	public static void main(String[] args) {
-		System.out.println(String.join(" ", 
-        " *** ", " *** ", " **** ", " ****"));
+		String[] lines = new String[7];
 
-System.out.println(String.join(" ", 
-        "*   *", "*   *", "*   *", "*    "));
+        lines[0] = String.join(" ", " *** ", " *** ", " **** ", " **** ");
+        lines[1] = String.join(" ", "*   *", "*   *", "*   *", "*     ");
+        lines[2] = String.join(" ", "*   *", "*   *", "*   *", "*     ");
+        lines[3] = String.join(" ", "*   *", "*   *", " **** ", " ***  ");
+        lines[4] = String.join(" ", "*   *", "*   *", "*     ", "    * ");
+        lines[5] = String.join(" ", "*   *", "*   *", "*     ", "    * ");
+        lines[6] = String.join(" ", " *** ", " *** ", "*     ", " **** ");
 
-System.out.println(String.join(" ", 
-        "*   *", "*   *", "*   *", "*    "));
-
-System.out.println(String.join(" ", 
-        "*   *", "*   *", " **** ", " *** "));
-
-System.out.println(String.join(" ", 
-        "*   *", "*   *", "*     ", "    *"));
-
-System.out.println(String.join(" ", 
-        "*   *", "*   *", "*     ", "    *"));
-
-System.out.println(String.join(" ", 
-        " *** ", " *** ", "*     ", " ****"));
+        for (String line : lines) {
+            System.out.println(line);
+		}
 	}
 }
